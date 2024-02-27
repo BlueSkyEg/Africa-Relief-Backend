fac4c13cebe776137caeb93d7feb85345723de39
Abdelrahman Ismael <a.ismael@bluskyint.com>
2024-02-27 11:24:19 +0200

    udpate 4 missed columns at donations table

--- helpers/backup-command.sql
+++ helpers/backup-command.sql
@@ -16,7 +16,7 @@
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             INDEX(email, login_name)
         );
-    -- Step 2: Insert data into the new 'users' table from the 'afj77_users' table
+    -- Step 2: Insert data into the new 'users' table from the 'afj77_users' table from old db
         INSERT INTO
             africa_relief.users (id, login_name, password, email, name)
         SELECT
@@ -40,9 +40,9 @@
             stripe_customer_id VARCHAR(255),
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            INDEX(email, stripe_customer_id)
+            INDEX(email, user_id, stripe_customer_id)
         );
-    -- Step 2: Insert data into the new 'donors' table from the 'afj77_give_donors' table
+    -- Step 2: Insert data into the new 'donors' table from the 'afj77_give_donors' table from old db
         INSERT INTO
             africa_relief.donors (id, user_id, email, stripe_customer_id)
         SELECT
@@ -86,7 +86,7 @@
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             INDEX(donor_id, donation_id)
         );
-    -- Step 2: Insert data into the new 'subscriptions' table from the 'afj77_give_subscriptions' table
+    -- Step 2: Insert data into the new 'subscriptions' table from the 'afj77_give_subscriptions' table from old db
         INSERT INTO
             africa_relief.subscriptions (id, donor_id, donation_id, stripe_subscription_id, period, amount, status, notes, start_date, end_date)
         SELECT
@@ -116,6 +116,7 @@
             id BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             donor_id BIGINT(20),
             subscription_id BIGINT(20) DEFAULT 0,
+            form_id BIGINT(20),
             project_title VARCHAR(255),
             amount DECIMAL(10,2),
             currency VARCHAR(255),
@@ -132,15 +133,20 @@
             zip VARCHAR(255),
             address1 VARCHAR(255),
             address2 VARCHAR(255),
+            comment VARCHAR(255),
+            completed_date VARCHAR(255),   -- Not (TIMESTAMP) because it sometimes retrieved null records
+            anonymous_donation VARCHAR(255),
+            cs_exchange_rate VARCHAR(10),
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             INDEX(donor_id, subscription_id)
         );
-    -- Step 2: Insert data into the new 'donations' table from the 'afj77_give_donationmeta' table
+    -- Step 2: Insert data into the new 'donations' table from the 'afj77_give_donationmeta' table from old db
         INSERT INTO africa_relief.donations (
             id,
             donor_id,
             subscription_id,
+            form_id,
             project_title,
             amount,
             currency,
@@ -148,20 +154,25 @@
             payment_mode,
             payment_gateway,
             payment_transaction_id,
+            phone,
             first_name,
             last_name,
-            phone,
             country,
             city,
             state,
             zip,
             address1,
-            address2
+            address2,
+            comment,
+            completed_date,
+            anonymous_donation,
+            cs_exchange_rate
         )
         SELECT
             afj77_give_donationmeta.donation_id AS id,
             MAX(CASE WHEN meta_key = '_give_payment_donor_id' THEN meta_value END) AS donor_id,
             MAX(CASE WHEN meta_key = 'subscription_id' THEN meta_value END) AS subscription_id,
+            MAX(CASE WHEN meta_key = '_give_payment_form_id' THEN meta_value END) AS form_id,
             MAX(CASE WHEN meta_key = '_give_payment_form_title' THEN meta_value END) AS project_title,
             MAX(CASE WHEN meta_key = '_give_payment_total' THEN meta_value END) AS amount,
             MAX(CASE WHEN meta_key = '_give_payment_currency' THEN meta_value END) AS currency,
@@ -169,15 +180,19 @@
             MAX(CASE WHEN meta_key = '_give_payment_mode' THEN meta_value END) AS payment_mode,
             MAX(CASE WHEN meta_key = '_give_payment_gateway' THEN meta_value END) AS payment_gateway,
             MAX(CASE WHEN meta_key = '_give_payment_transaction_id' THEN meta_value END) AS payment_transaction_id,
+            MAX(CASE WHEN meta_key = '_give_payment_donor_phone' THEN meta_value END) AS phone,
             MAX(CASE WHEN meta_key = '_give_donor_billing_first_name' THEN meta_value END) AS first_name,
             MAX(CASE WHEN meta_key = '_give_donor_billing_last_name' THEN meta_value END) AS last_name,
-            MAX(CASE WHEN meta_key = '_give_payment_donor_phone' THEN meta_value END) AS phone,
             MAX(CASE WHEN meta_key = '_give_donor_billing_country' THEN meta_value END) AS country,
             MAX(CASE WHEN meta_key = '_give_donor_billing_city' THEN meta_value END) AS city,
             MAX(CASE WHEN meta_key = '_give_donor_billing_state' THEN meta_value END) AS state,
             MAX(CASE WHEN meta_key = '_give_donor_billing_zip' THEN meta_value END) AS zip,
             MAX(CASE WHEN meta_key = '_give_donor_billing_address1' THEN meta_value END) AS address1,
-            MAX(CASE WHEN meta_key = '_give_donor_billing_address2' THEN meta_value END) AS address2
+            MAX(CASE WHEN meta_key = '_give_donor_billing_address2' THEN meta_value END) AS address2,
+            MAX(CASE WHEN meta_key = '_give_donation_comment' THEN meta_value END) AS comment,
+            MAX(CASE WHEN meta_key = '_give_completed_date' THEN meta_value END) AS completed_date,
+            MAX(CASE WHEN meta_key = '_give_anonymous_donation' THEN meta_value END) AS anonymous_donation,
+            MAX(CASE WHEN meta_key = '_give_cs_exchange_rate' THEN meta_value END) AS cs_exchange_rate
         FROM
             africa_relief_wp.afj77_give_donationmeta
         GROUP BY
