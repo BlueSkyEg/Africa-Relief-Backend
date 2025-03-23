90ae190d2459a8ef52cf8b700999fcd6552d736d
mosayeh99 <mohamedelsayeh66@gmail.com>
2025-03-23 19:44:27 +0200

    update thank email for donation

--- resources/views/mail/donation/received.blade.php
+++ resources/views/mail/donation/received.blade.php
@@ -1,11 +1,11 @@
 <x-mail::message>
 Dear {{ $name }},
 
-I hope this email finds you well. On behalf of {{ config('app.name') }}, I want to sincerely thank you for your generous donation of
-    {{ $amount }}. Your support means the world to us and helps us continue our mission.<br>
+We hope this email finds you well. On behalf of {{ config('app.name') }}, We want to sincerely thank you for your generous donation of
+    ${{ $amount }}. Your support means the world to us and helps us continue our mission.<br>
 
 Thank you again for being a part of our community. Your kindness and generosity make a real difference, and we are deeply grateful for your support.<br>
 
-With gratitude,<br>
+with gratitude,<br>
 {{ config('app.name') }}
 </x-mail::message>
